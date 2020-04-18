b8c6222ca6ad0dbb3c1c953996f03f9846edf7c8
Zhao Yiming <1225347740@qq.com>
2020-04-18 18:04:41 +0800

    save

---
+++ src/com/code4Book/HotSwapClassLoader.java
@@ -0,0 +1,13 @@
+package com.code4Book;
+
+public class HotSwapClassLoader extends ClassLoader {
+
+    public HotSwapClassLoader() {
+        super(HotSwapClassLoader.class.getClassLoader());
+    }
+
+    public Class loadByte(byte[] classByte) {
+        return defineClass(null, classByte, 0, classByte.length);
+    }
+
+}
